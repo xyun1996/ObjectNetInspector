57163c3d3370be9f2f33a046625a09dcdb55cd5c
xianyun <git_xy@hotmail.com>
2026-03-10 22:10:16 +0800

    Add explicit Window menu entry for Object Net Inspector tab

--- Source/ObjectNetInspector/Private/ObjectNetInspectorModule.cpp
+++ Source/ObjectNetInspector/Private/ObjectNetInspectorModule.cpp
@@ -2,6 +2,7 @@
 
 #include "Framework/Docking/TabManager.h"
 #include "Modules/ModuleManager.h"
+#include "ToolMenus.h"
 
 #if __has_include("WorkspaceMenuStructure.h") && __has_include("WorkspaceMenuStructureModule.h")
 #include "WorkspaceMenuStructure.h"
@@ -35,16 +36,47 @@ public:
 #endif
 
         UE_LOG(LogObjectNetInspector, Log, TEXT("ObjectNetInspector module started and tab spawner registered."));
+
+        UToolMenus::RegisterStartupCallback(
+            FSimpleMulticastDelegate::FDelegate::CreateRaw(this, &FObjectNetInspectorModule::RegisterMenus));
     }
 
     virtual void ShutdownModule() override
     {
+        if (UToolMenus* ToolMenus = UToolMenus::TryGet())
+        {
+            ToolMenus->UnregisterOwner(this);
+        }
+
         if (FModuleManager::Get().IsModuleLoaded(TEXT("Slate")))
         {
             FGlobalTabmanager::Get()->UnregisterNomadTabSpawner(ObjectNetInspectorTabId);
         }
 
         UE_LOG(LogObjectNetInspector, Log, TEXT("ObjectNetInspector module shutdown."));
+    }
+
+private:
+    void RegisterMenus()
+    {
+        UToolMenu* WindowMenu = UToolMenus::Get()->ExtendMenu(TEXT("LevelEditor.MainMenu.Window"));
+        if (WindowMenu == nullptr)
+        {
+            UE_LOG(LogObjectNetInspector, Verbose, TEXT("Failed to extend LevelEditor.MainMenu.Window."));
+            return;
+        }
+
+        FToolMenuSection& Section = WindowMenu->FindOrAddSection(TEXT("ObjectNetInspector"));
+        Section.AddMenuEntry(
+            TEXT("OpenObjectNetInspectorTab"),
+            FText::FromString(TEXT("Object Net Inspector")),
+            FText::FromString(TEXT("Open Object Net Inspector analysis tab.")),
+            FSlateIcon(),
+            FUIAction(
+                FExecuteAction::CreateLambda([]()
+                {
+                    FGlobalTabmanager::Get()->TryInvokeTab(ObjectNetInspectorTabId);
+                })));
     }
 };
 
